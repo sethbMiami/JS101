97ab2c91a6054d3b5a9c7595b648a24943c01ef0
Seth Bruner <bruner.seth@outlook.com>
2023-11-22 13:16:06 -0500

    added score, refactored game logic

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,8 +1,14 @@
+//global variables
+
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
 const QUICK_CHOICE = ['r', 'p', 's', 'sp', 'l'];
 
-let playAgainLoop = true;
+let playAgain = true;
+let numberOfGames = 0;
+let score = 0;
+
+//functions
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -25,51 +31,79 @@ function convertQuickToValid(quickChoice) {
 }
 
 function displayWinner(choice, computerChoice) {
-  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
 
   if (choice === computerChoice) {
-    prompt("It's a tie!");
+    return ("It's a tie!");
   } else if (((choice === 'rock' || choice === 'spock') && computerChoice === 'scissors') ||
   ((choice === 'paper' || choice === 'spock') && computerChoice === 'rock') ||
   ((choice === 'scissors' || choice === 'lizard') && computerChoice === 'paper') ||
   ((choice === 'paper' || choice === 'lizard') && computerChoice === 'spock') ||
   ((choice === 'scissors' || choice === 'rock') && computerChoice === 'lizzard')) {
-    prompt('You win!');
+    return ('You win!');
   } else {
-    prompt("Computer wins!");
+    return ("Computer wins!");
   }
 }
 
-while (playAgainLoop) {
+//game logic
+
+while (playAgain) {
+  prompt("WELCOME TO **RPS + Spock & Lizard** PLAY 5 ROUNDS - BEST OF 5 IS GRAND WINNER");
+  prompt("-".repeat(78));
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  prompt("Quick Choice: 'r' for rock, 'p' for paper, 's' for scissors, 'sp' for spock, or 'l' for lizard");
-  let choice = readline.question().toLowerCase();
 
-  while (!VALID_CHOICES.includes(choice) && !QUICK_CHOICE.includes(choice)) {
-    prompt("That's not a valid choice");
-    choice = readline.question().toLowerCase();
+  while (numberOfGames < 5) {
+    prompt("Quick Choice: 'r' for rock, 'p' for paper, 's' for scissors, 'sp' for spock, or 'l' for lizard");
+    let choice = readline.question().toLowerCase();
+
+    while (!VALID_CHOICES.includes(choice) && !QUICK_CHOICE.includes(choice)) {
+      prompt("That's not a valid choice");
+      choice = readline.question().toLowerCase();
+    }
+
+    if (QUICK_CHOICE.includes(choice)) {
+      choice = convertQuickToValid(choice);
+    }
+
+    console.clear();
+
+    let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+    let computerChoice = VALID_CHOICES[randomIndex];
+
+    let result = displayWinner(choice, computerChoice);
+    prompt(`${result} You chose ${choice}, computer chose ${computerChoice}`);
+
+    //if its a tie it doesn't count towards numberOfGames
+    if (result === "You win!") {
+      score++;
+      numberOfGames++;
+    } else if (result !== "It's a tie!") {
+      numberOfGames++;
+    } else {
+      prompt("Let's try that again.");
+    }
+
+    prompt(`Game ${numberOfGames} of 5`);
   }
 
-  if (QUICK_CHOICE.includes(choice)) {
-    choice = convertQuickToValid(choice);
+  if (score >= 3) {
+    prompt(`You won ${score} out of 5 games! You are the GRAND WINNER!!!!`);
+  } else {
+    prompt(`You only won ${score} out of 5 games. Better luck next time.`);
   }
-
-  console.clear();
-
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
-
-  displayWinner(choice, computerChoice);
 
   prompt('Do you want to play again (y/n)?');
   let answer = readline.question().toLowerCase();
+
   while (answer[0] !== 'n' && answer[0] !== 'y') {
     prompt('Please enter "y" or "n".');
     answer = readline.question().toLowerCase();
   }
 
   if (answer[0] !== 'y') {
-    playAgainLoop = false;
+    playAgain = false;
   }
+  score = 0;
+  numberOfGames = 0;
   console.clear();
 }
