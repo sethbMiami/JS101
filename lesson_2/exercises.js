5352bd928e5e59aa9a222fac056dab83f78ba155
Seth Bruner <bruner.seth@outlook.com>
2023-11-22 21:06:03 -0500

    solution files easy1

--- lesson_2/exercises.js
+++ lesson_2/exercises.js
@@ -30,3 +30,41 @@ function random () {
 random(newVar);
 console.log(newVar);
 
+function longestWord1(sentence) {
+  let words = sentence.split(' ');
+  let savedWord = words[0];
+
+  words.forEach(word => {
+    if (word.length >= savedWord.length) {
+      savedWord = word;
+    }
+  });
+
+  return savedWord;
+}
+
+function longestWord4(sentence) {
+  let words = sentence.split(' ');
+  let savedWord = words[0];
+
+  words.forEach(word => {
+    if (savedWord.length <= word.length) {
+      savedWord = word;
+    }
+  });
+
+  return savedWord;
+}
+
+console.log(Number("12.5"));
+console.log(parseInt("12.5"));
+
+let readline = require("readline-sync");
+
+function askYesOrNo(prompt) {
+  let answer = readline.question(prompt);
+  return answer;
+}
+
+let xyz = askYesOrNo([1, 2, 3]);
+console.log(xyz);
