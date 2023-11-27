4b18d93b485e89cd323784950970bab19c688552
Seth Bruner <bruner.seth@outlook.com>
2023-11-27 13:23:23 -0500

    answers to practiceproblems

--- lesson_3/Easy_3/PracticeProblems.js
+++ lesson_3/Easy_3/PracticeProblems.js
@@ -1,2 +1,12 @@
+/* eslint-disable max-len */
 //Write three different ways to remove all of the elements from the following array:
 
+let numbers = [1, 2, 3, 4];
+//numbers = [];
+//numbers.length = 0;
+numbers = numbers.slice(4);
+console.log(numbers);
+
+const isColorValid = color => ["blue", "green"].includes(color);
+console.log(isColorValid("red"));
+console.log(isColorValid("blue"));
